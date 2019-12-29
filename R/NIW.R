dea6a31815fa1364ed97d524b2afe1ba4607ba46
tfjaeger <fjaeger@ur.rochester.edu>
2019-12-29 17:59:11 -0500

    Bug fixes in get_expected_category_statistics

--- R/NIW.R
+++ R/NIW.R
@@ -19,6 +19,13 @@ NULL
 #' (representing uncertainty in the true value of \code{S_n}), we get
 #' \code{E[E[Sigma]] = mean(S_n / (nu_n - D - 1))}.
 #'
+#' @param x An mv_ibbu_stanfit or mv_ibbu_MCMC object.
+#' @param category Character vector with categories (or category) for which category statistics are to be
+#' returned.
+#' @param group Character vector with groups (or group) for which category statistics are to be
+#' returned.
+#' @param statistic Either `mu` for category mean or `Sigma` for category covariance matrix.
+#'
 #' @seealso TBD
 #' @keywords TBD
 #' @references Murphy, K. P. (2012). Machine learning: a probabilistic perspective. MIT press.
@@ -26,7 +33,8 @@ NULL
 #' TBD
 #' @rdname get_expected_category_statistics
 #' @export
-get_expected_category_statistics = function(x, statistic = c("mu", "Sigma"), category, group) {
+get_expected_category_statistics = function(x, category, group, statistic = c("mu", "Sigma")) {
+  assert_that(statistic %in% c("mu", "Sigma"))
   assert_that(is.mv_ibbu_stanfit(x) | is.mv_ibbu_MCMC(x, nested = T, long = T))
   if (is.mv_ibbu_stanfit(x))
     x = add_ibbu_draws(x, which = "both", wide = F, nest = T)
@@ -37,32 +45,34 @@ get_expected_category_statistics = function(x, statistic = c("mu", "Sigma"), cat
   assert_that(is.character(group) | is.numeric(group))
 
   x %<>%
-    filter(group %in% group, category %in% category)
+    filter(group %in% !! group, category %in% !! category)
 
-  if (statistic == "mean") {
+  if (statistic == "mu") {
     x %<>%
-    summarise(mu.mean = list(M %>% purrr::reduce(`+`) / length(M)))
-  } else {
+      group_by(group, category) %>%
+      summarise(mu.mean = list(M %>% purrr::reduce(`+`) / length(M)))
+  } else if (statistic == "Sigma") {
     D = dim(x$S[[1]])[1]
     x %<>%
       mutate(Sigma = map2(S, nu, ~ .x / (.y - D - 1))) %>%
+      group_by(group, category) %>%
       summarise(Sigma.mean = list(Sigma %>% purrr::reduce(`+`) / length(Sigma)))
-  }
+  } else stop("statistic must be either mu or Sigma.")
 
   # If just one category and group was requested, just return that object, rather
   # than the tibble
-  if (nrow(x) == 1) x = x[1,1][[1]][[1]]
+  if (nrow(x) == 1) x = x[,paste0(statistic, ".mean")][[1]][[1]]
   return(x)
 }
 
 #' @rdname get_expected_category_statistics
 #' @export
 get_expected_mu = function(x, category, group) {
-  return(get_expected_category_statistics(x, "mean", category, group))
+  return(get_expected_category_statistics(x, category, group, statistic = "mu"))
 }
 
 #' @rdname get_expected_category_statistics
 #' @export
 get_expected_sigma = function(x, category, group) {
-  return(get_expected_category_statistics(x, "Sigma", category, group))
+  return(get_expected_category_statistics(x, category, group, statistic = "Sigma"))
 }
