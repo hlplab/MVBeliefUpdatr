9f4c1f2dbc08eaf7398935d81d2ee31e5987e33f
tfjaeger <fjaeger@ur.rochester.edu>
2020-05-12 21:53:42 -0400

    Updated namespace and class definitions.

--- R/class.R
+++ R/class.R
@@ -20,13 +20,14 @@ is.mv_ibbu_stanfit = function(x) {
 #'
 #' Check whether \code{x} is a tibble of NIW beliefs.
 is.NIW_belief = function(x, is.long = T, category = "category") {
-  assert_that(is.flag(long) & is.long == T,
+  assert_that(is.flag(is.long) & is.long == T,
               msg = "Currently only NIW beliefs in long tibble format can be recognized.")
   flag = all(
     is_tibble(x),
-    c(!! sym(category),
+    c(category,
       "kappa", "nu", "M", "S") %in% names(x),
-    is.factor(x$category))
+    # Is category a factor?
+    is.factor(x %>% select(!! sym(category)) %>% unlist()))
 
 
   return(flag)
