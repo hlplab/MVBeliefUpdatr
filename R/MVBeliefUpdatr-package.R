cbfec3875d84ff062137cf7f3e5ccf8881e1e8ee
tfjaeger <fjaeger@ur.rochester.edu>
2020-07-17 15:09:17 -0400

    added package help file

---
+++ R/MVBeliefUpdatr-package.R
@@ -0,0 +1,10 @@
+#' @section Package options: TBD
+#' @keywords internal
+#' @references TBD
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
