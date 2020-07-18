fa2fc116da20cac3256edfbac9f97966e01cd1d7
tfjaeger <fjaeger@ur.rochester.edu>
2020-07-18 16:05:34 -0400

    Further updating of naming conventions.

--- tests/document_and_testthat.R
+++ tests/document_and_testthat.R
@@ -12,7 +12,7 @@ source("./R/visualize-fit.R")
 # Load data
 fit = readRDS(file = "./tests/test models/IBBU_mv_fit_3 exposure groups_2 categories_2 cues_Drouin et al 2016.rds")
 fit.input = readRDS(file = "./tests/test models/DATA_mv_fit_3 exposure groups_2 categories_2 cues_Drouin et al 2016.rds")
-  
+
 # class(fit) = "mv_ibbu_stanfit"
 categories = c("s", "sh")
 # groups = c("SH-BIAS", "SS-BIAS")
@@ -20,17 +20,17 @@ groups = c("SH-BIAS", "SS-BIAS", "Control")
 cues = c("PC1", "PC2")
 d = tidyr::crossing(
   category = factor(categories, levels = categories),
-  group = factor(groups, levels = groups), 
+  group = factor(groups, levels = groups),
   cue = cues,
   cue2 = cues
 )
 
 fit %<>%
-  recover_types(d) 
+  recover_types(d)
 fit %>%
-  add_ibbu_draws(wide = F, which = "both", draws = c(10, 5), nest = F) -> f
+  add_ibbu_stanfit_draws(wide = F, which = "both", draws = c(10, 5), nest = F) -> f
 fit %>%
-  add_ibbu_draws(wide = F, which = "both", draws = c(10, 5), nest = T) -> g
+  add_ibbu_stanfit_draws(wide = F, which = "both", draws = c(10, 5), nest = T) -> g
 
 
 
@@ -43,24 +43,24 @@ get_expected_category_statistic(g, c("s","sh"), c("prior", "Control"), c("mu", "
 
 source("./R/visualize-fit.R")
 group.colors = c("darkgray", "blue", "red", "black")
-plot_ibbu_parameters(fit, which = "both", n.draws = 2)
-plot_ibbu_parameters(fit, which = "both", n.draws = 5,
+plot_ibbu_stanfit_parameters(fit, which = "both", n.draws = 2)
+plot_ibbu_stanfit_parameters(fit, which = "both", n.draws = 5,
                      group.colors = group.colors)
-plot_ibbu_test_categorization(fit, fit.input,
+plot_ibbu_stanfit_test_categorization(fit, fit.input,
                               which = "both", summarize = T, n.draws = 10,
                               group.colors = group.colors)
-# should throw error: 
+# should throw error:
 # fit %>%
-#   add_ibbu_draws(wide = F, which = "both", draws = c(10, 5), nest = F) -> f
-# plot_expected_ibbu_categories_2D(f) 
+#   add_ibbu_stanfit_draws(wide = F, which = "both", draws = c(10, 5), nest = F) -> f
+# plot_expected_ibbu_stanfit_categories_2D(f)
 fit %>%
-  add_ibbu_draws(wide = F, which = "both", draws = c(10, 5), nest = T) -> g
+  add_ibbu_stanfit_draws(wide = F, which = "both", draws = c(10, 5), nest = T) -> g
 get_expected_category_statistic(g)
-plot_expected_ibbu_categories_2D(g, type = "contour")
+plot_expected_ibbu_stanfit_categories_2D(g, type = "contour")
 # should throw error since not fit.input is provided:
-# plot_expected_ibbu_categories_2D(g, type = "contour", plot.test = T)
-plot_expected_ibbu_categories_contour2D(g, fit.input, plot.test = F)
-plot_expected_ibbu_categories_2D(g, fit.input, type = "density", resolution = 5)
+# plot_expected_ibbu_stanfit_categories_2D(g, type = "contour", plot.test = T)
+plot_expected_ibbu_stanfit_categories_contour2D(g, fit.input, plot.test = F)
+plot_expected_ibbu_stanfit_categories_2D(g, fit.input, type = "density", resolution = 5)
 
 
 
@@ -72,6 +72,6 @@ plot_expected_ibbu_categories_2D(g, fit.input, type = "density", resolution = 5)
 
 # Not for test (slow! (at least the 2D density one))
 fit %>%
-  add_ibbu_draws(wide = F, which = "both", nest = T) -> g
-plot_expected_ibbu_categories_2D(g, fit.input)
-plot_expected_ibbu_categories_density2D(g, fit.input, resolution = 100)
+  add_ibbu_stanfit_draws(wide = F, which = "both", nest = T) -> g
+plot_expected_ibbu_stanfit_categories_2D(g, fit.input)
+plot_expected_ibbu_stanfit_categories_density2D(g, fit.input, resolution = 100)
