4424a5b384b50fd264d05ae419ff185dd5fb5730
tfjaeger <fjaeger@ur.rochester.edu>
2020-07-22 19:18:39 -0400

    Update

--- R/MVBeliefUpdatr-package.R
+++ R/MVBeliefUpdatr-package.R
@@ -1,7 +1,7 @@
 #' @import assertthat
 #' @importFrom magrittr %<>% %T>%
 #' @importFrom Hmisc %nin%
-#' @importFrom rlang !! !!! .data is_symbol sym syms expr as_name
+#' @importFrom rlang !! !!! .data is_symbol sym syms expr as_name quo_is_null
 #' @importFrom purrr map map2 pmap reduce
 #' @importFrom dplyr %>% select filter mutate mutate_at summarise summarise_at left_join rename rename_at group_by ungroup
 #' @importFrom tidyr crossing nest unnest
