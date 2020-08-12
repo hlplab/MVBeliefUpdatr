87cfcca0547df141c45c72ee6a1aee5414039344
tfjaeger <fjaeger@ur.rochester.edu>
2020-08-12 01:53:04 -0400

    debugging

--- R/compose-input-for-stan.R
+++ R/compose-input-for-stan.R
@@ -346,22 +346,22 @@ compose_data_to_infer_prior_via_conjugate_ibbu_w_sufficient_stats = function(
     m_0 = array(numeric(), dim = c(0,0))
     m_0_known = 0
   } else {
+    m_0_known = 1
     if (is.list(m_0)) {
       temp = array(dim = c(n.cats, n.cues))
       for (i in 1:length(m_0)) temp[i,] = m_0[[i]]
       m_0 = temp
-      m_0_known = 1
       rm(temp) }}
 
   if (is.null(S_0)) {
     S_0 = array(numeric(), dim = c(0,0,0))
     S_0_known = 0
   } else {
+    S_0_known = 1
     if (is.list(S_0)) {
       temp = array(dim = c(n.cats, n.cues, n.cues))
       for (i in 1:length(S_0)) temp[i,,] = S_0[[i]]
       S_0 = temp
-      S_0_known = 1
       rm(temp) }}
 
   if (length(cues) > 1) {
