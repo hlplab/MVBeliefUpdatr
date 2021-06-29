f04ff58463a9dc3272d1b0626befceea18e300be
T. Florian Jaeger <fjaeger@ur.rochester.edu>
2021-06-29 17:11:12 -0600

    overrode colMeans and cov to handle univariate cases

--- R/override.R
+++ R/override.R
@@ -4,7 +4,21 @@ NULL
 
 # Added here to handle the case of univariate categories
 #' @export
-rmvnorm = function (n, mean = rep(0, length(sigma)^.5), sigma = diag(length(mean)), ...) {
+colMeans <- function(x, ...) {
+  if (!is.array(x) || length(dim(x)) < 2L)
+    mean(x) else colMeans(x)
+}
+
+# Added here to handle the case of univariate categories
+#' @export
+cov <- function(x, ...) {
+  if (!is.array(x) || length(dim(x)) < 2L)
+    var(x) else cov(x)
+}
+
+# Added here to handle the case of univariate categories
+#' @export
+rmvnorm <- function (n, mean = rep(0, length(sigma)^.5), sigma = diag(length(mean)), ...) {
   if (length(mean) == 1)
     return(rnorm(n = n, mean = as.vector(mean), sd = as.vector(sigma^.5), ...)) else
       return(rmvnorm(n, mean, sigma, ...))
@@ -12,7 +26,7 @@ rmvnorm = function (n, mean = rep(0, length(sigma)^.5), sigma = diag(length(mean
 
 # Added here to handle the case of univariate categories
 #' @export
-rmvt = function (n, delta = rep(0, length(sigma)^.5), sigma = diag(length(mean)), df, ...) {
+rmvt <- function (n, delta = rep(0, length(sigma)^.5), sigma = diag(length(mean)), df, ...) {
   if (length(mean) == 1)
     # Using rlst instead of rt since rt is for standardized t distribution (no scale parameter)
     return(rlst(n = n, df = df, mu = as.vector(delta), sigma = as.vector(sigma^.5), ...)) else
@@ -21,14 +35,14 @@ rmvt = function (n, delta = rep(0, length(sigma)^.5), sigma = diag(length(mean))
 
 # Added here to handle the case of univariate categories
 #' @export
-dmvnorm = function (x, mean = rep(0, length(sigma)^.5), sigma = diag(length(mean)), ...) {
+dmvnorm <- function (x, mean = rep(0, length(sigma)^.5), sigma = diag(length(mean)), ...) {
   if (length(mean) == 1)
     return(dnorm(x = x, mean = as.vector(mean), sd = as.vector(sigma^.5))) else
       return(dmvnorm(x = x, mean, sigma, ...))
 }
 
 # Added here to handle the case of univariate categories
-dmvt = function (x, delta = rep(0, length(sigma)^.5), sigma = diag(length(mean)), df, ...) {
+dmvt <- function (x, delta = rep(0, length(sigma)^.5), sigma = diag(length(mean)), df, ...) {
   if (length(mean) == 1)
     # Using dlst instead of dt since dt is for standardized t distribution (no scale parameter)
     return(dlst(x = x, df = df, mu = as.vector(delta), sigma = as.vector(sigma^.5), ...)) else
