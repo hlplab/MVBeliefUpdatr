c623de50bed66060bf7bd4cc39ee948c54068e9f
T. Florian Jaeger <fjaeger@ur.rochester.edu>
2021-06-28 21:58:27 -0600

    debugging

--- R/override.R
+++ R/override.R
@@ -4,7 +4,7 @@ NULL
 
 # Added here to handle the case of univariate categories
 #' @export
-rmvnorm = function (n, mean = rep(0, nrow(sigma)), sigma = diag(length(mean)), ...) {
+rmvnorm = function (n, mean = rep(0, length(sigma)^.5), sigma = diag(length(mean)), ...) {
   if (length(mean) == 1)
     return(rnorm(n = n, mean = as.vector(mean), sd = as.vector(sigma^.5), ...)) else
       return(rmvnorm(n, mean, sigma, ...))
@@ -12,7 +12,7 @@ rmvnorm = function (n, mean = rep(0, nrow(sigma)), sigma = diag(length(mean)), .
 
 # Added here to handle the case of univariate categories
 #' @export
-rmvt = function (n, delta = rep(0, nrow(sigma)), sigma = diag(length(mean)), df, ...) {
+rmvt = function (n, delta = rep(0, length(sigma)^.5), sigma = diag(length(mean)), df, ...) {
   if (length(mean) == 1)
     # Using rlst instead of rt since rt is for standardized t distribution (no scale parameter)
     return(rlst(n = n, df = df, mu = as.vector(delta), sigma = as.vector(sigma^.5), ...)) else
@@ -21,14 +21,14 @@ rmvt = function (n, delta = rep(0, nrow(sigma)), sigma = diag(length(mean)), df,
 
 # Added here to handle the case of univariate categories
 #' @export
-dmvnorm = function (x, mean = rep(0, nrow(sigma)), sigma = diag(length(mean)), ...) {
+dmvnorm = function (x, mean = rep(0, length(sigma)^.5), sigma = diag(length(mean)), ...) {
   if (length(mean) == 1)
     return(dnorm(x = x, mean = as.vector(mean), sd = as.vector(sigma^.5))) else
       return(dmvnorm(x = x, mean, sigma, ...))
 }
 
 # Added here to handle the case of univariate categories
-dmvt = function (x, delta = rep(0, nrow(sigma)), sigma = diag(length(mean)), df, ...) {
+dmvt = function (x, delta = rep(0, length(sigma)^.5), sigma = diag(length(mean)), df, ...) {
   if (length(mean) == 1)
     # Using dlst instead of dt since dt is for standardized t distribution (no scale parameter)
     return(dlst(x = x, df = df, mu = as.vector(delta), sigma = as.vector(sigma^.5), ...)) else
