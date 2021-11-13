4062bb22f54c61c412cc09b97c3703c34065a6d4
tiflo <fjaeger@ur.rochester.edu>
2021-11-13 13:15:12 -0500

    EXTENDED get_categoriation to handle lapsing more consistently.

--- R/get-info-from-NIW-IA.R
+++ R/get-info-from-NIW-IA.R
@@ -7,29 +7,31 @@ get_categorization_function_from_NIW_ideal_adaptor = function(x, ...) {
     kappas = x$kappa,
     nus = x$nu,
     lapse_rate = x$lapse_rate,
-    bias = x$bias,
+    lapse_bias = x$lapse_bias,
     ...
   )
 }
 
-#' Get categorization from an NIW belief
+#' Get categorization from an NIW ideal adaptor
 #'
-#' Categorize a single observation based on an NIW belief. The decision rule can be specified to be either the
+#' Categorize a single observation based on an NIW ideal adaptor The decision rule can be specified to be either the
 #' criterion choice rule, proportional matching (Luce's choice rule), or the sampling-based interpretation of
 #' Luce's choice rule.
 #'
 #' @param x A vector of observations.
-#' @param belief An \code{\link[=is.NIW_belief]{NIW_belief}} object.
+#' @param model An \code{\link[=is.NIW_ideal_adaptor]{NIW_ideal_adaptor}} object.
 #' @param decision_rule Must be one of "criterion", "proportional", or "sampling".
-#' @param add_noise Determines whether multivariate Gaussian noise is added to the input.
-#' If `NULL`, no noise is added during the updating. If "sample" then a sample of
-#' noise is added to the input. If "marginalize" then each observation is transformed into the marginal distribution
+#' @param noise_treatement Determines how multivariate Gaussian noise is added to the input. Can be "sample" or
+#' "marginalize". If "sample", observations are adjusted by samples drawn from the noise distribution before applying
+#' categorization.If "marginalize" then each observation is transformed into the marginal distribution
 #' that results from convolving the input with noise. This latter option might be helpful, for example, if one is
-#' interested in estimating the consequences of noise across individuals. If add_noise is not `NULL` a Sigma_noise
-#' column must be present in the NIW_belief object specified as the priors argument. (default: `NULL`)
-#' @param add_lapse Determines the proportion of trials on which the listener lapses, not categorizing based on the
-#' percept. If add_lapse is "sample", the lapse rate and bias from the ideal adaptor will be used. If NULL, the lapse
-#' is assumed to be zero. (default: `NULL`)
+#' interested in estimating the consequences of noise across individuals. (default: "sample" if decision_rule is
+#' "sample"; "marginalize" otherwise).
+#' @param lapse_treatment Determines how lapses will be treated. Can be "sample" or "marginalize". If "sample", whether
+#' a trial is lapsing or not will be sampled for each observations. If a trial is sampled to be a lapsing trial the
+#' lapse biases are used as the posterior for that trial. If "marginalize", the posterior probability will be adjusted
+#' based on the lapse formula lapse_rate * lapse_bias + (1 - lapse_rate) * posterior probability from perceptual model.
+#' (default: "sample" if decision_rule is "sample"; "marginalize" otherwise).
 #' @param simplify Should the output be simplified, and just the label of the selected category be returned? This
 #' option is only available for the criterion and sampling decision rules. (default: `FALSE`)
 #'
@@ -46,96 +48,96 @@ get_categorization_from_NIW_ideal_adaptor = function(
   x,
   belief,
   decision_rule,
-  add_noise = NULL,
-  add_lapse = NULL,
+  noise_treatment = if (decision_rule == "sample") "sample" else "marginalize",
+  lapse_treatment = decision_rule,
   simplify = F
 ) {
   # TO DO: check dimensionality of x with regard to belief.
-  assert_NIW_belief(belief)
+  assert_NIW_ideal_adaptor(belief)
   assert_that(decision_rule  %in% c("criterion", "proportional", "sampling"),
               msg = "Decision rule must be one of: 'criterion', 'proportional', or 'sampling'.")
-  assert_that(any(is.null(add_noise), add_noise %in% c("sample", "marginalize")),
-              msg = 'add_noise must be one of "sample" or "marginalize"')
-  assert_that(any(is.null(add_noise), "Sigma_noise" %in% names(belief)),
-              msg = "Can't add noise: argument belief does not have column Sigma_noise.")
-  if (!is.null(add_noise)) {
-    assert_that(any(add_noise %in% "marginalize",
-                    all(add_noise %in% c("sample"), is_weakly_greater_than(length(x), 1))),
-                msg = "For noise sampling, x must be of length 1 or longer.")
+  assert_that(any(is.null(noise_treatement), noise_treatement %in% c("sample", "marginalize")),
+              msg = 'noise_treatement must be one of "sample" or "marginalize"')
+
+  # How should noise be treated?
+  if (!is.null(noise_treatement)) {
+    if (noise_treatement %in% c("sample"))
+      assert_that(
+        is_weakly_greater_than(length(x), 1),
+        msg = "For noise sampling, x must be of length 1 or longer.")
 
     # Handle noise
-    if (add_noise == "sample") {
-      x = x + rmvnorm(n = length(x), sigma = belief$Sigma_noise[[1]])
-    } else if (add_noise == "marginalize") message("Method 'marginalize' not yet implemented for add_noise.")
-  }
-  assert_that(any(is.null(add_lapse), is_scalar_character(add_lapse)),
-              msg = "add_lapse must be NULL or a character.")
-  if (is_scalar_character(add_lapse)) {
-    assert_that(add_lapse == "sample", msg = "If add_lapse is a character, it must be 'sample'.")
-    assert_that(assert_NIW_ideal_adaptor(belief), msg = "If add_lapse = 'sample', belief must be an  ideal adaptor.")
-    lapse_rate = unique(belief$lapse_rate)
-    lapse_biases = belief$bias
-  } else {
-    lapse_rate = 0
-    lapse_biases = rep(0, get_nlevels_of_category_labels_from_model(belief))
+    if (noise_treatement == "sample") {
+      x <- x + rmvnorm(n = length(x), sigma = belief$Sigma_noise[[1]])
+    } else if (noise_treatement == "marginalize") message("Method 'marginalize' not yet implemented for noise_treatement.")
   }
 
-  if (!is.list(x)) x <- list(x) # in case a single x is handed as argument
-  p = get_posterior_predictive_from_NIW_belief(x = x, belief = belief, log = F) %>%
+  # In case a single x is handed as argument, make sure it's made a list so that the length check below
+  # correctly treats it as length 1 (rather than the dimensionality of the one observation).
+  if (!is.list(x)) x <- list(x)
+  posterior_probabilities <-
+    get_posterior_predictive_from_NIW_belief(x = x, belief = belief, log = F) %>%
     group_by(category) %>%
     mutate(
-      observation = 1:length(x),
+      observationID = 1:length(x),
       x = x,
       prior = belief$prior[category]) %>%
-    group_by(observation) %>%
-    mutate(pp = (pp * prior) / sum(pp * prior))
+    group_by(observationID) %>%
+    mutate(posterior_probability = (posterior_predictive * prior) / sum(posterior_predictive * prior))
 
-  # Consider lapses
-  p %<>%
-    mutate(pp = ifelse(
-      rep(
-        rbinom(1, 1, lapse_rate),
-        get_nlevels_of_category_labels_from_model(belief)),
-      lapse_biases, # substitute lapse probabilities for posterior
-      pp))
+  # How should lapses be treated?
+  if (lapse_treatment  %in% c("sampling")) {
+    posterior_probabilities %<>%
+      mutate(
+        posterior_probability = ifelse(
+          rep(
+            rbinom(1, 1, lapse_rate),
+            get_nlevels_of_category_labels_from_model(belief)),
+          belief$lapse_bias[category],    # substitute lapse probabilities for posterior
+          posterior_probability))         # ... or not
+  } else {
+    posterior_probabilities %<>%
+      mutate(posterior_probability =  lapse_rate * lapse_bias + (1 - lapse_rate) * posterior_probability)
+  }
 
+  # Apply decision rule
   if (decision_rule == "criterion") {
-    p %<>%
+    posterior_probabilities %<>%
       mutate(
         # tie breaker in case of uniform probabilities
-        pp = ifelse(
+        posterior_probability = ifelse(
           rep(
-            sum(pp == max(pp)) > 1,
+            sum(posterior_probability == max(posterior_probability)) > 1,
             get_nlevels_of_category_labels_from_model(belief)),
-          pp + runif(
+          posterior_probability + runif(
             get_nlevels_of_category_labels_from_model(belief),
             min = 0,
             max = 0),
-          pp),
+          posterior_probability),
         # select most probable category
-        response = ifelse(pp == max(pp), 1, 0))
+        response = ifelse(posterior_probability == max(posterior_probability), 1, 0))
   } else if (decision_rule == "sampling") {
-    p %<>%
-      mutate(response = rmultinom(1, 1, pp) %>% as.vector())
+    posterior_probabilities %<>%
+      mutate(response = rmultinom(1, 1, posterior_probability) %>% as.vector())
   } else if (decision_rule == "proportional") {
-    p %<>%
-      mutate(response = pp)
-  }
+    posterior_probabilities %<>%
+      mutate(response = posterior_probability)
+  } else warning("Unsupported decision rule. This should be impossible to happen. Do not trust the results.")
 
-  p %<>%
+  posterior_probabilities %<>%
     ungroup() %>%
-    select(-pp) %>%
-    select(observation, x, category, response)
+    select(-c(posterior_predictive, posterior_probability)) %>%
+    select(observationID, x, category, response)
 
   if (simplify) {
     assert_that(decision_rule  %in% c("criterion", "sampling"),
                 msg = "For simplify = T, decision rule must be either criterion or sampling.")
-    return(p %>%
+    return(posterior_probabilities %>%
              filter(response == 1) %>%
-             select(observation, category) %>%
-             arrange(observation) %>%
+             select(observationID, category) %>%
+             arrange(observationID) %>%
              rename(response = category) %>%
              ungroup() %>%
              pull(response))
-  } else return(p)
+  } else return(posterior_probability)
 }
