212077efc47d2701f26151d22c4ddc679a7759db
tfjaeger <fjaeger@ur.rochester.edu>
2024-05-17 12:13:50 -0400

    added data

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#'
+NULL
+
+#' Chodroff & Wilson (2018) data on word-initial stop-voicing in native US English
+#'
+#' A subset of the data collected and annotated by Chodroff & Wilson (2018) of word-initial stop-voicing in native US English.
+#' Six stop categories ("b", "p", "d", "t", "g", "k") were recorded in two types of tasks that are known to elicit different
+#' speech styles: connected speech with faster speech rates, and isolated word productions with slower speech rates. The data
+#' contain annotations for three phonetic cues to stop-voicing: voice onset time (VOT), f0 (in either Hz or semitones), and
+#' vowel duration. For details, please see Chodroff & Wilson (2018), which should be cited if these data are used.
+#'
+#' Note that the present data are a {\em subset} of the data collected by Chodroff & Wilson (2018), and are not cleaned for issues
+#' like pitch doubling or halving. The full data are available on Eleanor Chodroff's OSF repo at \url{https://osf.io/jt5mc/}.
+#'
+#' @references
+#' \insertRef{chodroff-wilson2018}{Rdpack}
+#' @importFrom Rdpack reprompt
+#' @export
+"ChodroffWilson2018"
+
