2c038a54bb13ab8338c5962faad97c7ba3b933b1
tfjaeger <fjaeger@ur.rochester.edu>
2025-04-13 08:49:27 -0400

    updated benchmarking

--- tests/benchmarking.R
+++ tests/benchmarking.R
@@ -6,140 +6,70 @@ example <- 1
 seed <- 42
 .data <- make_data_for_stanfit(example, seed = seed)
 
-mb <- microbenchmark(
-  # Cholesky with transformation but without back-transformation
-  "cholesky, no transform" = infer_prior_beliefs(
-    exposure = .data %>% filter(Phase == "exposure"),
-    test = .data %>% filter(Phase == "test"),
-    cues =
-      if (example %in% 1:3)
-      {
-        c("VOT", "f0_semitones", "vowel_duration")[1:(example)]
-      } else if (example %in% 4:6) {
-        c("cue1", "cue2", "cue3")[1:(example %% 4 + 1)]
-      },
-    category = "category",
-    response = "Response",
-    group = "Subject",
-    group.unique = "Condition",
-    center.observations = F, scale.observations = F, pca.observations = F, transform_type = NULL, stanmodel = "mvg_conj_sufficient_stats_lapse_cholesky",
-    file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
-  "cholesky, center" = infer_prior_beliefs(
-    exposure = .data %>% filter(Phase == "exposure"),
-    test = .data %>% filter(Phase == "test"),
-    cues =
-      if (example %in% 1:3)
-      {
-        c("VOT", "f0_semitones", "vowel_duration")[1:(example)]
-      } else if (example %in% 4:6) {
-        c("cue1", "cue2", "cue3")[1:(example %% 4 + 1)]
-      },
-    category = "category",
-    response = "Response",
-    group = "Subject",
-    group.unique = "Condition",
-    center.observations = T, scale.observations = F, pca.observations = F, transform_type = NULL, stanmodel = "mvg_conj_sufficient_stats_lapse_cholesky",
-    file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
-  "cholesky, standardize" = infer_prior_beliefs(
-    exposure = .data %>% filter(Phase == "exposure"),
-    test = .data %>% filter(Phase == "test"),
-    cues =
-      if (example %in% 1:3)
-      {
-        c("VOT", "f0_semitones", "vowel_duration")[1:(example)]
-      } else if (example %in% 4:6) {
-        c("cue1", "cue2", "cue3")[1:(example %% 4 + 1)]
-      },
-    category = "category",
-    response = "Response",
-    group = "Subject",
-    group.unique = "Condition",
-    center.observations = T, scale.observations = T, pca.observations = F, transform_type = NULL, stanmodel = "mvg_conj_sufficient_stats_lapse_cholesky",
-    file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
-  # No Cholesky with transformation and back-transformation
-  "no cholesky, no transform" = infer_prior_beliefs(
-    exposure = .data %>% filter(Phase == "exposure"),
-    test = .data %>% filter(Phase == "test"),
-    cues =
-      if (example %in% 1:3)
-      {
-        c("VOT", "f0_semitones", "vowel_duration")[1:(example)]
-      } else if (example %in% 4:6) {
-        c("cue1", "cue2", "cue3")[1:(example %% 4 + 1)]
-      },
-    category = "category",
-    response = "Response",
-    group = "Subject",
-    group.unique = "Condition",
-    center.observations = NULL, scale.observations = NULL, pca.observations = NULL, transform_type = "identity", stanmodel = "mvg_conj_sufficient_stats_lapse",
-    file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
-  "no cholesky, center" = infer_prior_beliefs(
-    exposure = .data %>% filter(Phase == "exposure"),
-    test = .data %>% filter(Phase == "test"),
-    cues =
-      if (example %in% 1:3)
-      {
-        c("VOT", "f0_semitones", "vowel_duration")[1:(example)]
-      } else if (example %in% 4:6) {
-        c("cue1", "cue2", "cue3")[1:(example %% 4 + 1)]
-      },
-    category = "category",
-    response = "Response",
-    group = "Subject",
-    group.unique = "Condition",
-    center.observations = NULL, scale.observations = NULL, pca.observations = NULL, transform_type = "center", stanmodel = "mvg_conj_sufficient_stats_lapse",
-    file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
-  "no cholesky, standardize" = infer_prior_beliefs(
-    exposure = .data %>% filter(Phase == "exposure"),
-    test = .data %>% filter(Phase == "test"),
-    cues =
-      if (example %in% 1:3)
-      {
-        c("VOT", "f0_semitones", "vowel_duration")[1:(example)]
-      } else if (example %in% 4:6) {
-        c("cue1", "cue2", "cue3")[1:(example %% 4 + 1)]
-      },
-    category = "category",
-    response = "Response",
-    group = "Subject",
-    group.unique = "Condition",
-    center.observations = NULL, scale.observations = NULL, pca.observations = NULL, transform_type = "standardize", stanmodel = "mvg_conj_sufficient_stats_lapse",
-    file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
-  "no cholesky, PCA whiten" = infer_prior_beliefs(
-    exposure = .data %>% filter(Phase == "exposure"),
-    test = .data %>% filter(Phase == "test"),
-    cues =
-      if (example %in% 1:3)
-      {
-        c("VOT", "f0_semitones", "vowel_duration")[1:(example)]
-      } else if (example %in% 4:6) {
-        c("cue1", "cue2", "cue3")[1:(example %% 4 + 1)]
-      },
-    category = "category",
-    response = "Response",
-    group = "Subject",
-    group.unique = "Condition",
-    center.observations = NULL, scale.observations = NULL, pca.observations = NULL, transform_type = "PCA whiten", stanmodel = "mvg_conj_sufficient_stats_lapse",
-    file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
-  "no cholesky, ZCA whiten" = infer_prior_beliefs(
-    exposure = .data %>% filter(Phase == "exposure"),
-    test = .data %>% filter(Phase == "test"),
-    cues =
-      if (example %in% 1:3)
-      {
-        c("VOT", "f0_semitones", "vowel_duration")[1:(example)]
-      } else if (example %in% 4:6) {
-        c("cue1", "cue2", "cue3")[1:(example %% 4 + 1)]
-      },
-    category = "category",
-    response = "Response",
-    group = "Subject",
-    group.unique = "Condition",
-    center.observations = NULL, scale.observations = NULL, pca.observations = NULL, transform_type = "ZCA whiten", stanmodel = "mvg_conj_sufficient_stats_lapse",
-    file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
-  times = 20,
-  control = list(order = "random")
-)
+make_staninput_for_NIW_ideal_adaptor_with_transform <-
+  function(transform_type)
+    make_staninput_for_NIW_ideal_adaptor(
+      exposure = .data %>% filter(Phase == "exposure"),
+      test = .data %>% filter(Phase == "test"),
+      cues =
+        if (example %in% 1:3)
+        {
+          c("VOT", "f0_semitones", "vowel_duration")[1:(example)]
+        } else if (example %in% 4:6) {
+          c("cue1", "cue2", "cue3")[1:(example %% 4 + 1)]
+        },
+      category = "category",
+      response = "Response",
+      group = "Subject",
+      group.unique = "Condition",
+      transform_type = transform_type)
+
+mb <-
+  microbenchmark(
+    # Cholesky with transformation and back-transformation
+    "Cholesky, center" =
+      fit_NIW_ideal_adaptor(
+        staninput = make_staninput_for_NIW_ideal_adaptor_with_transform(transform_type = "center"),
+        stanmodel = "mvg_conj_sufficient_stats_lapse_cholesky",
+        file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
+    "Cholesky, standardize" =
+      fit_NIW_ideal_adaptor(
+        staninput = make_staninput_for_NIW_ideal_adaptor_with_transform(transform_type = "standardize"),
+        stanmodel = "mvg_conj_sufficient_stats_lapse_cholesky",
+        file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
+    "Cholesky, PCA whiten" =
+      fit_NIW_ideal_adaptor(
+        staninput = make_staninput_for_NIW_ideal_adaptor_with_transform(transform_type = "PCA whiten"),
+        stanmodel = "mvg_conj_sufficient_stats_lapse_cholesky",
+        file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
+    # No Cholesky with transformation and back-transformation
+    "no cholesky, no transform" =
+      fit_NIW_ideal_adaptor(
+        staninput = make_staninput_for_NIW_ideal_adaptor_with_transform(transform_type = "identity"),
+        stanmodel = "mvg_conj_sufficient_stats_lapse",
+        file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
+    "no cholesky, center" =
+      fit_NIW_ideal_adaptor(
+        staninput = make_staninput_for_NIW_ideal_adaptor_with_transform(transform_type = "center"),
+        stanmodel = "mvg_conj_sufficient_stats_lapse",
+        file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
+    "no cholesky, standardize" =
+      fit_NIW_ideal_adaptor(
+        staninput = make_staninput_for_NIW_ideal_adaptor_with_transform(transform_type = "standardize"),
+        stanmodel = "mvg_conj_sufficient_stats_lapse",
+        file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
+    "no cholesky, PCA whiten" =
+      fit_NIW_ideal_adaptor(
+        staninput = make_staninput_for_NIW_ideal_adaptor_with_transform(transform_type = "PCA whiten"),
+        stanmodel = "mvg_conj_sufficient_stats_lapse",
+        file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
+    "no cholesky, ZCA whiten" =
+      fit_NIW_ideal_adaptor(
+        staninput = make_staninput_for_NIW_ideal_adaptor_with_transform(transform_type = "ZCA whiten"),
+        stanmodel = "mvg_conj_sufficient_stats_lapse",
+        file = NULL, refresh = 400, iter = 2000, cores = 4, control = list(adapt_delta = 0.85)),
+    times = 25,
+    control = list(order = "random"))
 
 plot(mb)
 autoplot(mb)
